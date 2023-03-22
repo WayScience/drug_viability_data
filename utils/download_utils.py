56d35d4cea83006af0bcf9ff5db453e9c94424f6
Gregory Way <gregory.way@gmail.com>
2023-03-22 13:53:36 -0600

    replace urllibretreive with requests

--- utils/download_utils.py
+++ utils/download_utils.py
@@ -2,7 +2,7 @@
 Functions to facilitate downloading of specific publicly-available resources
 """
 
-import urllib.request
+import requests
 from zipfile import ZipFile
 import pathlib
 
@@ -11,6 +11,7 @@ def download_figshare(
     figshare_id: str,
     output_file: pathlib.Path,
     figshare_url: str = "https://ndownloader.figshare.com/files/",
+    chunk_size: int = 1024,
 ) -> pathlib.Path:
     """
     Download the provided figshare resource
@@ -23,12 +24,20 @@ def download_figshare(
         the location and file name of where to save the downloaded data
     figshare_url: str, default "https://ndownloader.figshare.com/files/"
         the location of where the figshare id is stored
+    chunk_size : int, default 1024
+        How many bytes to write at a given time to the output file, for requests stream
 
     Returns:
     --------
         The output file name
     """
-    urllib.request.urlretrieve(f"{figshare_url}/{figshare_id}", output_file)
+    download_url = f"{figshare_url}/{figshare_id}"
+
+    request_streamer = requests.get(download_url, stream=True)
+    with open(output_file, "wb") as output_file_writer:
+        for chunk in request_streamer.iter_content(chunk_size=chunk_size):
+            output_file_writer.write(chunk)
+
     return output_file
 
 
@@ -37,6 +46,7 @@ def download_depmap_bucket(
     output_dir: pathlib.Path,
     bucket: str = "depmap-external-downloads",
     resource: str = "pharmacological_profiling",
+    chunk_size: int = 1024,
 ) -> pathlib.Path:
     """
     Download a legacy depmap file not stored on figshare
@@ -51,6 +61,8 @@ def download_depmap_bucket(
         the name of the bucket where the DepMap data are stored
     resource: str, default "pharmacological_profiling"
         the category of DepMap resource
+    chunk_size : int, default 1024
+        How many bytes to write at a given time to the output file, for requests stream
 
     Returns:
     --------
@@ -63,7 +75,12 @@ def download_depmap_bucket(
 
     download_url = f"{base_url}{file_url}"
     output_file = pathlib.Path(output_dir, file_name)
-    urllib.request.urlretrieve(download_url, output_file)
+
+    request_streamer = requests.get(download_url, stream=True)
+    with open(output_file, "wb") as output_file_writer:
+        for chunk in request_streamer.iter_content(chunk_size=chunk_size):
+            output_file_writer.write(chunk)
+
     return output_file
 
 
@@ -76,6 +93,7 @@ def download_nci60(
     modification_date: str = "1672801037000",
     api: str = "v2",
     extract_zip: bool = False,
+    chunk_size: int = 1024,
 ) -> pathlib.Path:
     """
     Download the given nci-60 resource
@@ -98,6 +116,8 @@ def download_nci60(
         the version of the API used to download the file
     extract_zip, bool, default = False
         whether or not to extract the zip file
+    chunk_size : int, default 1024
+        How many bytes to write at a given time to the output file, for requests stream
 
     Returns:
     --------
@@ -106,7 +126,10 @@ def download_nci60(
     """
     request_string = f"{base_url}/{attachment_id}/{attachment_name}?version={version}&modificationDate={modification_date}&api={api}"
 
-    urllib.request.urlretrieve(request_string, output_file)
+    request_streamer = requests.get(request_string, stream=True)
+    with open(output_file, "wb") as output_file_writer:
+        for chunk in request_streamer.iter_content(chunk_size=chunk_size):
+            output_file_writer.write(chunk)
 
     if extract_zip:
         with ZipFile(output_file) as z:
